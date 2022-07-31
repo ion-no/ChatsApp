ae8d583c5dd9c7982a43347c48fe6216f4494d4d
Sriram <sriram135175@gmail.com>
2022-07-31 15:34:17 +0530

    revert

--- server/index.js
+++ server/index.js
@@ -1,19 +1,10 @@
 const express = require('express')
 const app = express()
-const handlebars = require("express-handlebars")
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const socketManage = require('./socketManage')(io)
 const PORT = process.env.PORT || 80
 const path = require('path')
-
-const socketsStatus = {};
-
-//config and set handlebars to express
-const customHandlebars = handlebars.create({ layoutsDir: "./views" });
-
-app.engine("handlebars", customHandlebars.engine);
-app.set("view engine", "handlebars");
 
 io.on('connection', socketManage )
 // In dev mode just hide hide app.uss(... ) below
--- server/socketManage.js
+++ server/socketManage.js
@@ -6,36 +6,6 @@ let communityChat = methods.createChat()
 let chats = [ communityChat ]
 
 module.exports = io => socket => {
-    
-    const socketId = socket.id;
-socketsStatus[socket.id] = {};
-
-
-console.log("connect");
-
-socket.on("voice", function (data) {
-
-    var newData = data.split(";");
-    newData[0] = "data:audio/ogg;";
-    newData = newData[0] + newData[1];
-
-    for (const id in socketsStatus) {
-
-    if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
-        socket.broadcast.to(id).emit("send", newData);
-    }
-
-})
-
-    socket.on("userInformation", function (data) {
-        socketsStatus[socketId] = data;
-
-        io.sockets.emit("usersUpdate", socketsStatus);
-    })
-    
-    socket.on("disconnect", function () {
-        delete socketsStatus[socketId];
-    })
 
     socket.on( events.IS_USER, ( nickname, cb ) => {
         methods.isUser( users, nickname ) ? cb({ isUser: true, user: null }) :
